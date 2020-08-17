a0ec50a2cdfa7262ead156fc5636bd1860c234df
DenilsonDonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-08-17 09:34:04 -0300

    alguns exemplos de teste

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -20,4 +20,41 @@ describe("Filter function", () => {
     //Teste
     expect(filterByTerm(input, "link")).toEqual(output);
   });
+
+
+  test("it should not care UPERCASE filter by a search term (link)", () => {
+    //Entradas
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" },
+    ];
+
+    //Saida
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+    //Teste
+    expect(filterByTerm(input, "LINK")).toEqual(output);
+  });
+
+  test("it should filter by a search term (link) but has to be https", () => {
+    //Entradas
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" },
+      { id: 4, url: "http://www.link3.dev" },
+      { id: 5, url: "HTTPS://WWW.URL1.DEV" },
+      { id: 6, url: "HTTPS://WWW.LINK6.DEV" },
+    ];
+
+    //Saida
+    const output = [{ id: 3, url: "https://www.link3.dev" },
+                    { id: 6, url: "https://www.link6.dev" }];
+
+    //Teste
+    expect(filterByTerm(input, "LINK")).toEqual(output);
+  });
+
+
 });
--- module.js
+++ module.js
@@ -1,7 +1,21 @@
 function filterByTerm(inputArr, searchTerm) {
-  return inputArr.filter(function (arrayElement) {
-    return arrayElement.url.match(searchTerm);
+    var inputArr2 = inputArr.map(function (arrayElement) {
+    return {"id":arrayElement.id,
+      "url":arrayElement.url.toLowerCase()}
+  })
+  var arrFilter = filterInputHttp(inputArr2);
+  console.log(inputArr2);
+  return arrFilter.filter(function (arrayElement) {
+    return arrayElement.url.toLowerCase().match(searchTerm.toLowerCase());
   });
 }
+
+function filterInputHttp(inputArr) {
+  return inputArr.filter(function (arrElement) {
+    console.log(arrElement);
+    return arrElement.url.toLowerCase().match('https');
+  })
+}
+
 
 module.exports = filterByTerm;
